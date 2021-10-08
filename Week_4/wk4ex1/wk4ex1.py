2f4fa0826abe5f6423cc2276702eb49e689a8041
Timo <timokosse@live.nl>
2021-10-08 15:05:32 +0200

    finished wk4ex1.py

--- Week_4/wk4ex1/wk4ex1.py
+++ Week_4/wk4ex1/wk4ex1.py
@@ -326,10 +326,19 @@ def echo(filename, timedelay):
     """
     print('Het orginele geluid:')
     play(filename)
-
+    
     sound_data = [0,0]
     read_wav(filename, sound_data)
-    
+    samps = sound_data[0]
+    sr = sound_data[1]
+    samplesilence = int(timedelay * sr)
+    echosamps = [0 for i in range(samplesilence)]
+    echosamps = echosamps + samps
+    new_samps = add_scale_2(echosamps, samps, 0.5, 0.5)
+    new_sound_data = [new_samps, sr]
+    write_wav(new_sound_data, 'out.wav')
+
+    play('out.wav')
 
 
 # Hulpfunctie om pure tonen te genereren
@@ -362,10 +371,50 @@ def pure_tone(freq, time_in_seconds):
     gen_pure_tone(freq, time_in_seconds, sound_data)
 
     print("De nieuwe geluidsgegevens opslaan...")
-    write_wav(new_sound_data, "out.wav")  # schrijf gegevens naar out.wav
+    write_wav(sound_data, "out.wav")  # schrijf gegevens naar out.wav
 
     print("Nieuw geluid afspelen...")
     play('out.wav')
 
 
 # Te schrijven geluidsfunctie #6: chord
+def chord(f1, f2, f3, timeSeconds):
+    """chord neemt 3 frequenties en een tijd en geeft een akkoord terug
+
+    Args:
+        f1 (float): frequentie 1
+        f2 (float): frequentie 2
+        f3 (fload): frequentie 3
+        timeSeconds (float): tijd in seconden
+    """
+
+    samps1, sr1 = gen_pure_tone(f1, timeSeconds, [0,0])
+    samps2, sr2 = gen_pure_tone(f2, timeSeconds, [0,0])
+    samps3, sr3 = gen_pure_tone(f3, timeSeconds, [0,0])
+
+    new_samps = add_scale_3(samps1, samps2, samps3, 0.33, 0.33, 0.33)
+    new_sound_data = [new_samps, sr1]
+    write_wav(new_sound_data, 'out.wav')
+
+    play('out.wav')
+
+    #chord(440.0, 554, 659, 1.0) A-majoor ! 
+
+
+def add_scale_3(L,m,n,L_scale,m_scale,n_scale):
+    """add_scale_3 neemt 3 lijsten en geeft 1 gescalde lijst  terug
+
+    Args:
+        L (list): lijst 1
+        m (list): lijst 2
+        n (list): lijst 3
+        L_scale (float): 1e scale
+        m_scale (float): 2e scale
+        n_scale (float): 3e scale
+    """
+    L = scale(L,L_scale)
+    m = scale(m,m_scale)
+    n = scale(n,n_scale)
+
+    return add_3(L,m,n)
+    
