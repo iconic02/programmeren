571d187f442e575b715368f4010fdc4bcbd83534
Timo <timokosse@live.nl>
2021-11-23 14:50:40 +0100

    WK7EX1

--- Week_7/wk7ex1/wk7ex1c.py
+++ Week_7/wk7ex1/wk7ex1c.py
@@ -1,46 +1,136 @@
 #
-# wk7ex1c.py - uniekheid controleren  (voor de random-number generator in Hmmm)
-#    De functie test(s) staat hier al in (onderaan).
+# wk7ex1c.py
 #
-# Naam:
+# Naam:Timo
 #
-# Je plakt je 100 getallen in deze triple-quoted string:
+# Getallen bij a = 21 & c = 17:
 NUMBERS = """
+99
+96
+33
+10
+27
+84
+81
+18
+95
+12
+69
+66
 3
-42
-47
-46
+80
+97
+54
+51
+88
+65
+82
+39
+36
+73
+50
+67
+24
+21
+58
+35
+52
+9
+6
+43
+20
+37
+94
 91
+28
 5
+22
+79
+76
+13
+90
+7
+64
+61
+98
+75
+92
+49
+46
+83
+60
+77
+34
+31
+68
+45
+62
+19
+16
+53
+30
+47
+4
+1
+38
+15
+32
+89
+86
+23
+0
+17
+74
+71
+8
+85
+2
+59
+56
+93
+70
+87
+44
+41
+78
+55
+72
+29
+26
+63
+40
+57
+14
+11
+48
+25
+42
 """
 
 
 def unique(L):
     """
-    This should be your uniqueness-tester, written for week 7
-    Usually, it uses the recursive pattern:
-
-    if ...      # handle base case
-    elif ...    # check whether L[0] re-appears
-    else ...    # otherwise...
+    Een test om te kijken of elk cijfer niet nog een keer in de lijst voorkomt
+    Als dit het geval is, wordt False gegeven, anders True
     """
-    return False     # dummycode
+    if L == []:
+        return True
+    elif L[0] in L[1:]:
+        return False
+    else:
+        return unique(L[1:])
 
 
 def test(s):
-    """test accepts a triple-quoted string, s,
-       containing one number per line. Then, test
-       returns True if those numbers are all unique
-       (or if s is empty); otherwise it returns False
     """
-    s = s.strip()                 # haal alle spaties aan het begin en eind van s weg
-    list_of_strings = s.split()   # splits s op elke spatie en nieuwe regel
-    # print("list_of_strings is", list_of_string)
-    list_of_integers = [int(s) for s in list_of_strings]  # converteer ze allemaal naar ints
-    # print("list_of_integers is", list_of_integers)
+    Een functie die van string NUMBERS een int list maakt en deze naar unique stuurt
+    """
+    s = s.strip()                 
+    list_of_strings = s.split()   
+    list_of_integers = [int(s) for s in list_of_strings]
     return unique(list_of_integers)
 
 
-# Uitproberen!
+#door het py bestand te runnen, wordt de string NUMBERS automatisch getest
 result = test(NUMBERS)
 print("\nTest op uniekheid:  Het resultaat is", result)
