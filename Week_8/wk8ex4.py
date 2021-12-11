c89021fb02630aa61dad847fb43eb2cf89f3ad5c
Timo <timokosse@live.nl>
2021-12-11 13:06:45 +0100

    Week acht klaar

--- Week_8/wk8ex4.py
+++ Week_8/wk8ex4.py
@@ -1,37 +1,59 @@
 #wk8ex4
 #conway probleem
 
-def conway(n):
-    start = '1'
-    placehold = ''
+def next(n):
+    """next neemt een int n. En converteerd deze naar het 'volgende' getal d.m.v. de conway methode.
+    zo wordt bijv 11242 --> 21121412.
+
+    Args:
+        n (int): een willekeurig getal.
+    """
+    n = str(n)
     count = 0
-    if start == '' or n < 0:
-        return 0
-    if n == 0:
-        return 1
-    while n > 0:
-        count += 1
-        if len(start) > 1:
-            if start[0] == start[1]:
-                start = start.replace(start[0], '', 1)
+    storage = '' #hier houden we de tot nu toe geconverteerde nummers
+    while n != '':
+        for i in n:
+            if i == n[0]:
+                count += 1
             else:
-                placehold = convert(start[0], count)
-                start = start.replace(start[0], '', 1)
-                count = 0
-        else:
-            placehold = convert(start[0], count)
+                break
+        storage += extra(n, count)
+        n = n.replace(n[0:count], '', 1)
+        count = 0
+    return int(storage)
+        
+    
+def extra(n, count):
+    """extra neemt n0 van next en count, en maakt hier een conway getal van
+
+    Args:
+        n (string): het cijfer als een string
+        count (int): het aantal keer dat ie voorkomt
+    """
+    result = str(count) + n
+    return result
+        
+
+assert next(21) == 1211
+assert next(2222) == 42
+assert next(312211) == 13112221
+
+
+
+def read_it(n):
+    """read_it drukt de eerste n getallen van conway's methode af
+
+    Args:
+        n (int): hoevaak moeten we afdrukken
+    """
+    start = 1
+    while n > 0:
+        print(start)
+        start = next(start)
         n -= 1
-    return start
-        
 
-def convert(num, count):
-    return str(count) + str(num)
-        
+    
 
 
-
-
-# split alle opeenvolgende nummers van elkaar:
-# 111221 wordt 111, 22, 1
     
 
