333838ee908f9ba5f0be578c6f8a089568d9d2e2
tim <timokosse@live.nl>
2021-12-20 18:13:39 +0100

    wk8 resubmit

--- Week_8/wk8ex4.py
+++ Week_8/wk8ex4.py
@@ -9,17 +9,23 @@ def next(n):
         n (int): een willekeurig getal.
     """
     n = str(n)
-    count = 0
+    count = 1
     storage = '' #hier houden we de tot nu toe geconverteerde nummers
     while n != '':
-        for i in n:
-            if i == n[0]:
-                count += 1
+        while True:
+            if len(n) > 1:
+                if n[1] == n[0]:
+                    count += 1
+                    n = n[1:]
+                else:
+                    storage += extra(n[0],count)
+                    n = n[1:]
+                    count = 1
+                    break
             else:
+                storage += extra(n[0],count)
+                n = n[1:]
                 break
-        storage += extra(n, count)
-        n = n.replace(n[0:count], '', 1)
-        count = 0
     return int(storage)
         
     
@@ -36,7 +42,9 @@ def extra(n, count):
 
 assert next(21) == 1211
 assert next(2222) == 42
+assert next(42) == 1412
 assert next(312211) == 13112221
+assert next(2229428) == 3219141218
 
 
 
